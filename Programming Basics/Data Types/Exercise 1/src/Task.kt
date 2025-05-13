6a390f30cdb1ffa39766b44c42bb4e4ef56d2d53
PC <juandi1704@gmail.com>
2025-05-12 22:59:30 -0500

    exercise 2 module3

--- Programming Basics/Data Types/Exercise 1/src/Task.kt
+++ Programming Basics/Data Types/Exercise 1/src/Task.kt
@@ -1,6 +1,30 @@
 // DataTypes/Task1.kt
 package dataTypesExercise1
 
+// 🔹 Step 1: Declare with type Int, then assign a String
+//      Type mismatch: inferred type is String but Int was expected
+//fun main() {
+//    var answer: Int = 42
+//    answer = "unknown"
+//    println(answer)
+//}
+
+// 🔹 Step 2: Remove the Int type (let Kotlin infer the type)
+//      Type mismatch: inferred type is String but Int was expected
 fun main() {
-  // TODO
+    // TODO
+    var answer = 42
+    answer = "unknown"
+    println(answer)
 }
+
+
+
+//fun main() {
+////    var answer:Int= 42;
+//    var answer= 42;
+//    answer.toString();
+////    answer = "unknown";
+//    println(answer)
+//
+//}
