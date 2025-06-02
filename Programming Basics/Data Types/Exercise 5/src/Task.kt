d2ec50b8f09ab1649d3fdbd528fd534f3b369867
PC <juandi1704@gmail.com>
2025-06-01 22:22:37 -0500

    exercise6 module data types

--- Programming Basics/Data Types/Exercise 5/src/Task.kt
+++ Programming Basics/Data Types/Exercise 5/src/Task.kt
@@ -2,6 +2,8 @@
 package dataTypesExercise5
 
 fun main() {
-  val s = "Sally" + 5.9
+  // Put the caret right after s and press Alt+Enter.
+  // This is the shortcut for Show intention actions. Choose Specify type explicitly
+  val s: String = "Sally" + 5.9
   println(s)
 }
